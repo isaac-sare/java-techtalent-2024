d668ecc57164b8c99b631a83b906021879346910
Isaac <isaacsaezreverte@gmail.com>
2024-02-02 21:49:36 +0100

    treball divendres vespre

--- TreballAutònom/src/Test/PonderacióNumsVariables.java
+++ TreballAutònom/src/Test/PonderacióNumsVariables.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class PonderacióNumsVariables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
 	Scanner teclado;
-	double n1, n2, n3;
+	double n1, n2, n3, n4;
 	teclado = new Scanner(System.in);
 	System.out.print("Introdueix la nota del primer parcial: ");
 	n1=teclado.nextInt();
 	System.out.print("Introdueix la nota del segon parcial: ");
 	n2 = teclado.nextInt();
 	System.out.print("Introdueix la nota del final: ");
-	n3 = teclado.nextInt();
+	n3 = teclado.nextDouble();
+
 	
-	System.out.print( "La seva suma ponderada és:" );
+	
+	System.out.print( "La suma ponderada és:" );
 	System.out.println(n1*0.2+n2*0.3+n3*0.5);
 	
 	if (n1*0.2+n2*0.3+n3*0.5>=9)
-		System.out.println("Molt bé, has tret un excel·lent. FELICITATS!");
-		if (n1*0.2+n2*0.3+n3*0.5>=5 && n1*0.2+n2*0.3+n3*0.5<7)
-		System.out.println("Has aprovat.");
-		if (n1*0.2+n2*0.3+n3*0.5>=7 && n1*0.2+n2*0.3+n3*0.5<9)
-			System.out.println("Has tret un notable. Segueix així!");
-		if (n1*0.2+n2*0.3+n3*0.5>=0 && n1*0.2+n2*0.3+n3*0.5<5)
-			System.out.println("Has suspès, t'has d'esforçar més");
+	System.out.println("Molt bé, has tret un excel·lent. FELICITATS!");
+	if (n1*0.2+n2*0.3+n3*0.5>=5 && n1*0.2+n2*0.3+n3*0.5<7)
+	System.out.println("Has aprovat.");
+	if (n1*0.2+n2*0.3+n3*0.5>=7 && n1*0.2+n2*0.3+n3*0.5<9)
+	System.out.println("Has tret un notable. Segueix així!");
+	if (n1*0.2+n2*0.3+n3*0.5>=0 && n1*0.2+n2*0.3+n3*0.5<5)
+	System.out.println("Has suspès, t'has d'esforçar més");
 	}
 
 }
